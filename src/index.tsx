b0a5394e2989ab92bd618d6f4d5eb22f26106d0a
garrettLoveUGuys <garrett@iloveuguys.org>
2026-07-05 22:40:12 -0500

    update page config logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { HomePage } from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
 import { ServicesPage } from "./pages/ServicesPage";
 import { ProjectsPage } from "./pages/ProjectsPage";
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/about", element: <AboutPage /> },
-  { path: "/services", element: <ServicesPage /> },
-  { path: "/projects", element: <ProjectsPage /> },
-]);
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <HomePage /> },
+    { path: "/about", element: <AboutPage /> },
+    { path: "/services", element: <ServicesPage /> },
+    { path: "/projects", element: <ProjectsPage /> },
+  ],
+  {
+    basename: "/client-1",
+  }
+);
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && screenGraphPlugin()],
-  base: "./",
+  base: "/client-1/",
   css: {
     postcss: {
       plugins: [tailwind()],
